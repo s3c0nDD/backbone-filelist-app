bb7333a86ff4a59daf4c9a46920118f0f0a2207c
s3c0nDD <dejfir@gmail.com>
2016-05-30 23:52:05 +0200

    working checkboxes add

--- app/views/file-view.js
+++ app/views/file-view.js
@@ -16,40 +16,40 @@ module.exports = Backbone.View.extend({
     el: "#app",
     counter: 0,
     events: {
-        'click button#new': 'changeTableHeader',
-        'click button#cancel': 'changeTableHeaderBack',
-        'click button#save': 'addItem'
+        // 'click button#new': 'changeTableHeader',
+        // 'click button#cancel': 'changeTableHeaderBack',
+        // 'click button#save': 'addItem'
     },
     files : [
         {"name": "file0"},
         {"name": "file1"},
         {"name": "file2"},
-        {"name": "file3"}
+        {"name": "file3"},
+        {"name": "file4"}
     ],
 
     initialize: function(){
-        // Files counter for the list
+        // files counter for the list
         this.counter = 0;
-        // Make mocks
+        // make mocks
         this.collection = new fileModel.FilesCollection();
         // this.collection.bind('add', this.renderItem());
         this.makeMocks();
-        // Not all views are self-rendering. This one is.
+        // not all views are self-rendering. This one is
         this.render();
     },
 
     render: function(){
         // add table header
         this.$el.find('#files-table-head').replaceWith(tableHeaderNormal());
-
-        // var string = newFileTemplate({ id: '0', name: 'example file'});
         // render views for all items in collection
         for(var i = 0; i < this.collection.size(); i++){
             var itemId = this.collection.at(i).get('id');
             var itemName = this.collection.at(i).get('name');
             this.renderItem(itemId, itemName);
-
         }
+        // set checkboxes listener
+        this.checkboxListener();
     },
 
     makeMocks: function () {
@@ -82,12 +82,68 @@ module.exports = Backbone.View.extend({
             this.collection.add(item);
             // Render view
             this.renderItem(this.counter, input);
-            this.changeTableHeaderBack();
+            // this.changeTableHeaderBack();
         }
     },
     
     renderItem: function (item_id, item_name) {
         var string = newFileTemplate({ id: item_id, name: item_name});
-        this.$el.find('tr:last').after(string);
+        this.$el.find('tbody').append(string);
+    },
+
+    checkboxListener: function () {
+        var $rename_btn = $('#rename'),
+            $delete_btn = $('#delete'),
+            $checkbox_all = $('#checkbox-all'),
+            $tbody = $('tbody'),
+            $all_rows = $tbody.find(':checkbox');
+
+        // listener for main checkbox change
+        $checkbox_all.on('change', function() {
+            var $this = $(this);
+            if($this.is(':checked')) {
+                // if was checked then check all other
+                $tbody.find(':checkbox').prop('checked', true).addClass('isChecked');
+                // ..and set 'rename' and 'delete' buttons
+                $rename_btn.removeClass('disabled').addClass('active');
+                $delete_btn.removeClass('disabled').addClass('active');
+            }
+            else if ($this.not(":checked")) {
+                // if was unchecked then uncheck all others..
+                $tbody.find(':checkbox').prop('checked', false).removeClass('isChecked');
+                // // ...and set 'rename' and 'delete' buttons
+                $rename_btn.removeClass('active').addClass('disabled');
+                $delete_btn.removeClass('active').addClass('disabled');
+            }
+        });
+
+        // listener for row checkboxes change
+        $tbody.find(':checkbox').on('change', function () {
+            var $this = $(this);
+            if ( $this.is(':checked') )
+                $this.prop('checked', true).addClass('isChecked');
+            else if ( $this.not(':checked') )
+                $this.prop('checked', false).removeClass('isChecked');
+            var checked_rows = $tbody.find(':checkbox:checked');
+            // if all checkboxes are NOT selected..
+            if ( checked_rows.length == 0 ) {
+                // ..then just disable buttons
+                $rename_btn.removeClass("active").addClass("disabled");
+                $delete_btn.removeClass("active").addClass("disabled");
+            }
+            // if any checkbox is checked we have to do things and even check more...
+            if ( checked_rows.length != 0 ) {
+                // firstly enable buttons
+                $rename_btn.removeClass('disabled').addClass('active');
+                $delete_btn.removeClass('disabled').addClass('active');
+                // then: check if all table rows checkboxes are checked...
+                console.log("Lenght:" + $all_rows.length + " and checked:" + checked_rows.length);
+                // ...so we can change main checkbox accordingly
+                if ($all_rows.length == checked_rows.length)
+                    $checkbox_all.prop('checked', true).addClass('isChecked');
+                else
+                    $checkbox_all.prop('checked', false).removeClass('isChecked');
+            }
+        });
     }
 });
